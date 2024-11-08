7994338b52e9897709bf1738f0ec4df23da60109
Prasad Anumala <prasad.anumala@microblocklabs.com>
2024-11-08 19:26:54 +0530

    initial-commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-slate-400">
+      <h1 className="text-orange-600 hover:text-green-400 hover:text-3xl">
+        This is a heading
+      </h1>
+    </div>
+  );
+};
+
+export default App;
