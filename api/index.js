464d8fd29babd24b00b34ea986154bb2eb2a3edc
prasadvihu <prasad.vihu@gmail.com>
2024-11-09 16:24:28 +0530

    mongo db connected

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Mongo DB connected"));
 
 app.listen(3000, () => console.log("Server is listening...!!!"));
